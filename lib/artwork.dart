6bdd922654ee885a863a4d61b59fc4a65959b0db
Gibitop <ilya.ruwork@gmail.com>
2019-08-19 01:32:19 +0300

    TODOs added

--- lib/artwork.dart
+++ lib/artwork.dart
@@ -3,8 +3,14 @@ part 'artwork.g.dart';
 
 const String dataNotAvaliable = 'Data not available';
 
+// To build a part file use "flutter pub run build_runner build" command
 @JsonSerializable()
 class Artwork {
+  // TODO: Add field "creation date"
+  // TODO: Add field "type"
+  // TODO: Add field "objects and themes"
+  // TODO: Add field "description"
+  
   String name = dataNotAvaliable;
   String size = dataNotAvaliable;
   String price = dataNotAvaliable;
