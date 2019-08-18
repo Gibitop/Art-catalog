2afc0014ed5450d3194adb0eeab1a4cf09308809
Gibitop <ilya.ruwork@gmail.com>
2019-08-19 01:32:48 +0300

    Product page redesign (work in progress)

--- lib/product.dart
+++ lib/product.dart
@@ -20,66 +20,80 @@ class Product extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: <Widget>[
-          Center(
-            child: Container(
-              child: FutureBuilder(
-                future: image,
-                builder: (context, snapshot) {
-                  return snapshot.data != null
-                      ? Image.file(snapshot.data)
-                      : Container(
-                          child: Icon(
-                            Icons.image,
-                            size: 40,
-                          ),
-                          padding: EdgeInsets.all(8),
-                        );
-                },
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                child: FutureBuilder(
+                  future: image,
+                  builder: (context, snapshot) {
+                    return snapshot.data != null
+                        ? Image.file(snapshot.data)
+                        : Container(
+                            child: Icon(
+                              Icons.image,
+                              size: 40,
+                            ),
+                            padding: EdgeInsets.all(8),
+                          );
+                  },
+                ),
+                constraints: BoxConstraints(
+                    minWidth: MediaQuery.of(context).size.width,
+                    maxHeight: MediaQuery.of(context).size.width),
+                margin: EdgeInsets.all(10),
               ),
-              constraints: BoxConstraints(
-                  minWidth: MediaQuery.of(context).size.width,
-                  maxHeight: MediaQuery.of(context).size.width),
-              margin: EdgeInsets.all(10),
             ),
-          ),
-          // Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  artwork.name,
-                  style: Theme.of(context).textTheme.headline,
-                ),
-                Text(
-                  artwork.price,
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-                Table(
-                  children: <TableRow>[
-                    TableRow(children: <Widget>[
-                      Text(
-                        'Size:',
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                      Text(artwork.size)
-                    ]),
-                    TableRow(children: <Widget>[
-                      Text(
-                        'Technique:',
-                        style: Theme.of(context).textTheme.subtitle,
-                      ),
-                      Text(artwork.technique)
-                    ]),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+            // Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    artwork.name,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                  Text(
+                    artwork.price,
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Table(
+                    defaultColumnWidth: FractionColumnWidth(.1),
+                    children: <TableRow>[
+                      TableRow(children: <Widget>[
+                        Text(
+                          'Size:',
+                          style: Theme.of(context).textTheme.body2,
+                        ),
+                        Text(
+                          artwork.size,
+                          style: Theme.of(context).textTheme.body1,
+                        )
+                      ]),
+                      TableRow(children: <Widget>[
+                        Text(
+                          'Technique:',
+                          style: Theme.of(context).textTheme.body2,
+                        ),
+                        Text(
+                          artwork.technique,
+                          style: Theme.of(context).textTheme.body1,
+                        )
+                      ]),
+                      // TODO: Add other fields
+                    ],
+                  ),
+                  // TODO: Add an artwork description
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
